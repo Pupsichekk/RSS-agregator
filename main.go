ae4aacfc81e32de1eba80e3bc56d6ce542d4f35c
Pupsichekk <www.sopin-l@mail.ru>
2024-01-11 04:47:47 +0400

    env fixed

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("Port is undefined in env")
+	}
+	fmt.Println("Port", portString)
 }
